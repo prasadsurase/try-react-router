d186f1de67d748864c8bd589fc594105af571ead
Prasad Surase <prasadsurase@gmail.com>
2017-01-31 23:00:21 +0530

    Added basic routes for About and Repos

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class App extends Component {
   render() {
@@ -6,6 +7,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React</h2>
+          <ul role="nav">
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/repos">Repos</Link></li>
+          </ul>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './components/App';
+import About from './components/About';
+import Repos from './components/Repos';
 
 ReactDOM.render((
   <Router history={ hashHistory }>
     <Route path='/' component={ App }/>
+    <Route path='/about' component={ About }/>
+    <Route path='/repos' component={ Repos }/>
   </Router>
   ),document.getElementById('root')
 );

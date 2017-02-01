5b2406e76cae1c356bcfb5f9c0e125e6ed6a8351
Prasad Surase <prasadsurase@gmail.com>
2017-02-01 21:21:22 +0530

    Added redirection in custom action

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import NavLink from './NavLink';
+import { browserHistory } from 'react-router';
 
 class Repos extends Component {
+  handleSubmit(event){
+    event.preventDefault();
+    var userName = event.target.elements[0].value;
+    var repo = event.target.elements[1].value;
+    var path = `/repos/${userName}/${repo}`;
+    console.log(path);
+    browserHistory.push(path);
+  }
+
   render(){
     return(
       <div>
@@ -9,6 +19,13 @@ class Repos extends Component {
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         { this.props.children }
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './components/App';
 import About from './components/About';
 import Repos from './components/Repos';
@@ -9,7 +9,7 @@ import Home from './components/Home';
 
 ReactDOM.render(
   (
-    <Router history={ hashHistory }>
+    <Router history={ browserHistory }>
       <Route path='/' component={ App }>
         <IndexRoute component={ Home }/>
         <Route path='/repos' component={ Repos }>
